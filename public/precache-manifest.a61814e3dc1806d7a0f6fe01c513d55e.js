59f1cc17b64d2664928695f4a5f6748da6725fef
svensone <svenson.ertel@gmail.com>
2019-02-16 15:28:10 +0800

    'build'

---
+++ public/precache-manifest.a61814e3dc1806d7a0f6fe01c513d55e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "bbfa51fc82231897a756",
+    "url": "/static/js/main.bbfa51fc.chunk.js"
+  },
+  {
+    "revision": "0c3488dcf7f49aabded6",
+    "url": "/static/js/2.0c3488dc.chunk.js"
+  },
+  {
+    "revision": "bbfa51fc82231897a756",
+    "url": "/static/css/main.a688451e.chunk.css"
+  },
+  {
+    "revision": "ef3027793b815af6c3f0333922bb5a51",
+    "url": "/index.html"
+  }
+];
