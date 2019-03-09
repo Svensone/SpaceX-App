a68f92d557e24f1bd1cc50215f61f006e3b99dd8
svensone <svenson.ertel@gmail.com>
2019-03-09 18:15:09 +0800

    set new app name in heroku

---
+++ public/build/precache-manifest.0bdbde5ffb8b49cabbdf4f49ffce2f08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f7fb84d088d628bc2538",
+    "url": "/static/js/main.f7fb84d0.chunk.js"
+  },
+  {
+    "revision": "0c3488dcf7f49aabded6",
+    "url": "/static/js/2.0c3488dc.chunk.js"
+  },
+  {
+    "revision": "f7fb84d088d628bc2538",
+    "url": "/static/css/main.874bdce0.chunk.css"
+  },
+  {
+    "revision": "69aa829486fe652b1dc60551830a79c6",
+    "url": "/index.html"
+  }
+];
